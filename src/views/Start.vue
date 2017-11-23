ce6d7a027ce753d4e29a2cf680d5f4be056ae01d
maxrgus <gusmaxigu@student.gu.se>
2017-11-23 19:48:27 +0100

    scaffolded layout

---
+++ src/views/Start.vue
@@ -0,0 +1,16 @@
+<template>
+  <section id="start">
+    <h1>Candle Team Gullegris</h1>
+    <div class="actions">
+      <a>Create a new game</a>
+      <input type="text" placeholder="Game id">
+      <a>Join an existing game</a>
+    </div>
+  </section>
+</template>
+<script>
+
+</script>
+<style scoped lang="scss">
+
+</style>
