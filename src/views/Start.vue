6d03461f324eb4191aa6c9c688600788bd033577
maxrgus <gusmaxigu@student.gu.se>
2017-11-23 19:50:11 +0100

    Removed redundant input type

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -3,7 +3,7 @@
     <h1>Candle Team Gullegris</h1>
     <div class="actions">
       <a>Create a new game</a>
-      <input type="text" placeholder="Game id">
+      <input placeholder="Game id">
       <a>Join an existing game</a>
     </div>
   </section>
