1706e4e1edebb1ae3618d116c8d6b8e407458825
Pregmark <Sam.pregmark@gmail.com>
2017-11-23 21:19:48 +0100

    Added end screen text

--- src/views/End.vue
+++ src/views/End.vue
@@ -1,9 +1,29 @@
-<template>
-    <div></div>
+<template xmlns:color="http://www.w3.org/1999/xhtml" xmlns:color="http://www.w3.org/1999/xhtml"
+          xmlns:color="http://www.w3.org/1999/xhtml">
+  <section id="end">
+    <div>
+      <h1>Game has ended</h1>
+      <p>{{ gameResult }}</p>
+    </div>
+  </section>
 </template>
 
 <script>
-    export default {}
+    export default {
+      data () {
+        return {
+        }
+      },
+      computed: {
+        gameResult () {
+          if (true) {
+            return "You didn't lose!"
+          } else {
+            return "You lost"
+          }
+        }
+      }
+    }
 </script>
 
 <style rel="stylesheet/scss" type="text/css" lang="scss" scoped></style>
